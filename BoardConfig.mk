690f1f3332e621021d5b7010c41045711e48ee55
Dmitry <42121541+dkpost3@users.noreply.github.com>
2022-10-07 14:51:02 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,9 +32,6 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a55
 
-# Assert
-TARGET_OTA_ASSERT_DEVICE := lisa,lisa_in,lisa_cn,lisa_global
-
 # A/B
 AB_OTA_UPDATER := true
 
